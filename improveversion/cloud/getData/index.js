ca6798a4cf5b712390ffc0e808381f6df367e687
loveleaves <56626062+loveleaves@users.noreply.github.com>
2020-11-28 08:19:48 +0800

    update 2020.11.28

--- improveversion/cloud/getData/index.js
+++
@@ -1,9 +0,0 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-cloud.init({
-  env: 'programprogram-3gq6d76z4f2f205f' 
- })
-// 云函数入口函数
-exports.main = async(event, context) => {
-  return await cloud.database().collection('login_info').get();
-}
